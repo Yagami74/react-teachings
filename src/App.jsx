89c468a20cd4eb0a9987ca17ae2ff1a09e844ac1
Yagami <samirdangal4@gmail.com>
2026-09-06 07:45:57 +0545

    perfect forms by react

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import EmployeeForm from "./component/FormLearnings/EmployeeRegistration/EmployeeForm";
+import CreateProduct from     "./component/FormLearnings/ProductForm/CreateProduct";
+import EventRegistration from "./component/FormLearnings/EventRegistration/EventRegistration";
 function App() {
   return (
     <>
       <EmployeeForm />
+      <CreateProduct />
+      <EventRegistration/>
     </>
   );
 }
